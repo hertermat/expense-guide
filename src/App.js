cffee8376450e02761e7bd6d66b3e82e69cf6e73
Matias Herter <matias.herter@georgebrown.ca>
2023-01-09 11:55:37 -0500

    Passing data up to App.js. Tiding code

--- src/App.js
+++ src/App.js
@@ -11,10 +11,15 @@ function App() {
     {id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021,2,28)},
     {id: 'e4', title: 'New Desk', amount: 450, date: new Date(2021, 5,12)}
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('in app.js');
+    console.log(expense);
+  }
   return (
     <div>
       
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses}></Expenses>
 
     </div>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,57 +1,88 @@
 import React, {useState} from "react";
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
-    /*
+const ExpenseForm = (props) => {
+    
     const [enteredTitle,setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-     Can Be used but might run into problems depending on complexity of code*/
+    // Can Be used but might run into problems depending on complexity of code
 
+    /*
     const [userInput, setUserInput] = useState({
         enteredTitle: '',
         entredAmount: '',
         enteredDate: ''
     });
+    cleaner way to use useState() but a bit more complex
+    */
 
      
     const titleChangeHandler = (event) => {
-        //setEnteredTitle(event.target.value) <<< Can be used, but might run into some problems depending on complexity of code
+        setEnteredTitle(event.target.value) //Can be used, but might run into some problems depending on complexity of code
+        /*
         setUserInput((prevState) => {
             return {...prevState, enteredTitle: event.target.value};
-        });
+        }); << // keeps it always up to date. always getting newest version
+        Better and cleaner way to use useState() but a bit more complex
+        */
     };
     const amountChangeHandler = (event) => {
-        // keeps it always up to date. always getting newest version
-        setUserInput((prevState) => {
-            return {...prevState, enteredAmount: event.target.value};
-        });
+        setEnteredAmount(event.target.value)
     };
     const dateChangeHandler = (event) => {
-        setUserInput((prevState) => {
-            return {...prevState, enteredDate: event.target.value};
-        });
+        setEnteredDate(event.target.value)
     };
 
-    return <form>
+    // By standart, page will reload once the button is clicked, but we can prevent that from happening.
+    //
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        props.onSaveExpenseData(expenseData);
+        //adding it to reset the fields of the form to an empty string after we click the submit add expense
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+ 
+    return (<form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
+                <input
+                 type='text' 
+                 value={enteredTitle} 
+                 onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
+                <input
+                 type='number' 
+                 min="0.01" 
+                 step="0.01" 
+                 value={enteredAmount} 
+                 onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+                <input
+                 type='date' 
+                 min="2019-01-01" 
+                 max="2023-12-31" 
+                 value={enteredDate} 
+                 onChange={dateChangeHandler} />
             </div>
         </div>
         <div className="new-expense__actions">
             <button type='submit'>Add Expense</button>
+            
         </div>
-    </form>
+    </form>)
 };
 
 export default ExpenseForm;
